5caad67d177601bcfcdcc579a980273e16d27280
Thales Nathan <thcmatias@gmail.com>
2021-02-01 17:24:01 -0300

    Implementing index route

--- app/Http/Controllers/API/V1/ProductController.php
+++ app/Http/Controllers/API/V1/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -10,17 +12,17 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return ProductResource
      */
     public function index()
     {
-        //
+        return new ProductResource(Product::paginate(10));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
