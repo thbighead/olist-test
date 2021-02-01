5163650e5b910fc7ad2cac749ad8617e1f73c84e
Thales Nathan <thcmatias@gmail.com>
2021-02-01 17:22:51 -0300

    Defining model
    Just fillable fields and casting types

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'sku' => 'string',
+        'price' => 'decimal:2',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'sku',
+        'name',
+        'description',
+        'price',
+    ];
 }
