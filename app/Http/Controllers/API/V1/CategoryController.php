efef8a68b09314d9300a4bb708d9ddb60a64e8f7
Thales Nathan <thcmatias@gmail.com>
2021-02-01 18:28:04 -0300

    Implementing endpoints

--- app/Http/Controllers/API/V1/CategoryController.php
+++ app/Http/Controllers/API/V1/CategoryController.php
@@ -3,62 +3,85 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return CategoryResource
      */
     public function index()
     {
-        //
+        return new CategoryResource(Category::paginate(10));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreCategoryRequest $request
+     * @return \Illuminate\Http\JsonResponse|object
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $newCategory = new Category;
+
+        $success = $newCategory->fill($request->only($newCategory->getFillable()))
+            ->save();
+
+        return (new CategoryResource($newCategory))->additional([
+            'success' => $success,
+        ])->response()->setStatusCode($success ? 200 : 500);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return CategoryResource
      */
-    public function show($id)
+    public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param UpdateCategoryRequest $request
+     * @param Category $category
+     * @return \Illuminate\Http\JsonResponse|object
      */
-    public function update(Request $request, $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $oldCategory = clone $category;
+
+        $success = $category->update($request->only($category->getFillable()));
+
+        return (new CategoryResource($category))->additional([
+            'success' => $success,
+            'old' => $oldCategory->toArray(),
+        ])->response()->setStatusCode($success ? 200 : 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return \Illuminate\Http\JsonResponse|object
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $success = (bool)$category->delete();
+
+        return (new CategoryResource($category))
+            ->additional(['success' => $success])
+            ->response()
+            ->setStatusCode($success ? 200 : 500);
     }
 }
