0e5eac93d0cc176764756e7cca155a239c25636b
Thales Nathan <thcmatias@gmail.com>
2021-02-02 13:40:07 -0300

    Defining Category resource response structure

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,6 +14,13 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'products_amount' => $this->products_count,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
     }
 }
