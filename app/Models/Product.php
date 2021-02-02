f3c51723464664eec3e8a690df22bbf95af197f8
Thales Nathan <thcmatias@gmail.com>
2021-02-02 13:37:48 -0300

    Redefining Entities seeder
    As we added a relatioship between products and categories entities, our seeders needed to be updated accordingly.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,36 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $sku
+ * @property string $name
+ * @property string $description
+ * @property mixed $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property int $category_id
+ * @property-read \App\Models\Category $category
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Product onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereSku($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Product withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Product withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
@@ -16,6 +46,7 @@ class Product extends Model
      * @var array
      */
     protected $casts = [
+        'category_id' => 'integer',
         'sku' => 'string',
         'price' => 'decimal:2',
     ];
@@ -26,9 +57,15 @@ class Product extends Model
      * @var array
      */
     protected $fillable = [
+        'category_id',
         'sku',
         'name',
         'description',
         'price',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,11 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
+    const MIN_PRODUCTS_PER_CATEGORY = 0;
+    const MAX_PRODUCTS_PER_CATEGORY = 50;
+
     /**
      * Run the database seeds.
      *
@@ -14,6 +18,21 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory()->count(500)->create();
+        foreach (Category::all() as $category) {
+            if ($category->id === 1) {
+                continue; // forcing skipping first category to have an example without products
+            }
+
+            $how_many = rand(self::MIN_PRODUCTS_PER_CATEGORY, self::MAX_PRODUCTS_PER_CATEGORY);
+
+            if ($how_many === 0) {
+                continue;
+            }
+
+            Product::factory()
+                ->count($how_many)
+                ->for($category)
+                ->create();
+        }
     }
 }
