eb355d20afeb31da35971e73ba55ba650dd4a93f
Thales Nathan <thcmatias@gmail.com>
2021-02-03 01:19:54 -0300

    Fixing category store endpoint http code return
    • Tests\Feature\V1CategoryTest > store endpoint
      Response status code [200] does not match expected 201 status code.
      Failed asserting that 200 is identical to 201.

--- app/Http/Controllers/API/V1/CategoryController.php
+++ app/Http/Controllers/API/V1/CategoryController.php
@@ -42,7 +42,7 @@ class CategoryController extends Controller
 
         return (new CategoryResource($newCategory))->additional([
             'success' => $success,
-        ])->response()->setStatusCode($success ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR);
+        ])->response()->setStatusCode($success ? Response::HTTP_CREATED : Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 
     /**
