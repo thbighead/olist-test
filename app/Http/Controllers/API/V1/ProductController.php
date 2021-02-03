74e113edbb1aea268813254bcd7442aac3b6b948
Thales Nathan <thcmatias@gmail.com>
2021-02-03 02:37:48 -0300

    Finxing loading
    Not count, we do want the category relationship to be loaded

--- app/Http/Controllers/API/V1/ProductController.php
+++ app/Http/Controllers/API/V1/ProductController.php
@@ -118,7 +118,7 @@ class ProductController extends Controller
     {
         $destroyedProduct = Product::withTrashed()->findOrFail($id)->load(['category']);
         $success = (bool)$destroyedProduct->restore();
-        $destroyedProduct->loadCount(['category']);
+        $destroyedProduct->load(['category']);
 
         return (new ProductResource($destroyedProduct))
             ->additional(['success' => $success])
@@ -134,7 +134,7 @@ class ProductController extends Controller
      */
     public function forceDestroy(Product $product)
     {
-        $product->loadCount(['category']);
+        $product->load(['category']);
 
         $success = (bool)$product->forceDelete();
 
